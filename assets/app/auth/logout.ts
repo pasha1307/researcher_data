c82d6169b48f80e1be10efa4165c36e041d50f5b
Pavel Krotov <pavelkrotov@Pavels-MBP.home>
2017-03-21 13:06:09 -0400

    logut redirect

--- assets/app/auth/logout.ts
+++ assets/app/auth/logout.ts
@@ -1,12 +1,14 @@
 import {Component} from "@angular/core";
 import {AuthService} from "./auth.service";
+import {Router} from "@angular/router";
 @Component({
     selector: 'app-logout',
     template: `<button type="button" class="btn btn-info" (click)="logout()">Logout</button>`
 })
 export class LogoutComponent {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 logout() {
         this.authService.logout();
+        this.router.navigateByUrl('/signup');
 }
 }
